bd43696fe7ab4bfcf225e5cf9d7d9b62f583bde4
Sherlock-White <1172940427@qq.com>
2022-11-26 15:44:59 +0800

    add animator & shader graph

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -7,14 +7,27 @@ public class PlayerController : MonoBehaviour
 {
     private NavMeshAgent agent;
 
+    private Animator anim;
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
 
     private void Start()
     {
         MouseManager.Instance.OnMouseClicked += MoveToTarget;
+    }
+
+    private void Update()
+    {
+        SwitchAnimation();
+    }
+
+    public void SwitchAnimation()
+    {
+        anim.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 
     public void MoveToTarget(Vector3 target)

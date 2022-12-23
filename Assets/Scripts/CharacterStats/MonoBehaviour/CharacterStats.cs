7576f59fc4be76ca93e66a038977b37cda9cb5a1
Sherlock-White <1172940427@qq.com>
2022-12-23 14:48:31 +0800

    add UI

--- Assets/Scripts/CharacterStats/MonoBehaviour/CharacterStats.cs
+++ Assets/Scripts/CharacterStats/MonoBehaviour/CharacterStats.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class CharacterStats : MonoBehaviour
 {
+    public event Action<int, int> UpdateHealthBarOnAttack;
     public CharacterData_SO templateData;
     public CharacterData_SO characterData;
     public AttackData_SO attackData;
@@ -50,14 +51,21 @@ public class CharacterStats : MonoBehaviour
         {
             defender.GetComponent<Animator>().SetTrigger("Hit");
         }
-        //TODO: Update UI
-        //TODO: 经验update
+        //Update UI
+        UpdateHealthBarOnAttack?.Invoke(CurrentHealth,MaxHealth);
+        //经验update
+        if(CurrentHealth <= 0)
+            attacker.characterData.UpdateExp(characterData.killPoint);
     }
 
     public void TakeDamage(int damage,CharacterStats defender)
     {
         int currentDamage = Mathf.Max(damage - defender.CurrentDefence, 0);
         CurrentHealth = Mathf.Max(CurrentHealth - currentDamage, 0);
+        UpdateHealthBarOnAttack?.Invoke(CurrentHealth, MaxHealth);
+
+        if (CurrentHealth <= 0)
+            GameManager.Instance.playerStats.characterData.UpdateExp(characterData.killPoint);
     }
 
     private int CurrentDamage()
